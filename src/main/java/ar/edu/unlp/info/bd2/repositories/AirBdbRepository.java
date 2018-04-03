6ede0c590ec7f3d0b9727edd0fb4912634198b1b
agustin <ortu.agustin@gmail.com>
2018-04-03 10:25:17 -0300

    intentando buscar usuario

--- src/main/java/ar/edu/unlp/info/bd2/repositories/AirBdbRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/AirBdbRepository.java
@@ -5,6 +5,7 @@ import ar.edu.unlp.info.bd2.model.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
@@ -18,17 +19,23 @@ public class AirBdbRepository {
 
   public <T> void save(T object) {
     Session sess = sessionFactory.getCurrentSession();
-    Transaction tx;
+    Transaction tx = null;
     try {
       tx = sess.beginTransaction();
       sess.save(object);
       tx.commit();
-    }
-    catch (Exception e) {
-      if (tx!=null) tx.rollback();
+    } catch (Exception e) {
+      if (tx != null)
+        tx.rollback();
       throw e;
-    }
-    finally {
+    } finally {
       sess.close();
     }
+  }
+
+  public User getUserByUsername(String email) {
+    return (User) sessionFactory.getCurrentSession().createCriteria(User.class)
+        .add(Restrictions.eq("email", email)).list()
+        .get(0);
+  }
 }
